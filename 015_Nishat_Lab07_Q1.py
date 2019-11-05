c59c69a16279b3590d687371801408d411882a9a
Nishat Wasi <57230638+nishatWasi@users.noreply.github.com>
2019-11-05 22:23:44 +0530

    Add files via upload
    Sorry

---
+++ 015_Nishat_Lab07_Q1.py
@@ -0,0 +1,23 @@
+#Roll no.:
+f = open("integers.txt","r")
+while True:
+    line = f.readline()
+    try:
+        line = int(line)
+        if line%2 == 0:
+            f1 = open("even.txt", "a")
+            f1.write(str(line))
+            f1.write("\n")
+            f1.close()
+        else:
+            f2 = open("odd.txt","a")
+            f2.write(str(line))
+            f2.write("\n")
+            f2.close()
+        if not line:
+            break
+    except ValueError:
+        break
+print(f1.name)
+print(f2.name)
+f.close()
